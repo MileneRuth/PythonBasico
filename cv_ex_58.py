0141e15bac97757e1450b4be8960540a64de5509
MileneRuth <milene.ruth.ruth@outlook.com>
2025-12-17 12:38:10 -0300

    joguinho de palpite
    utilizando o while w o randint

--- cv_ex_58.py
+++ cv_ex_58.py
@@ -1,4 +1,17 @@
-print('vou pensar em um número de 0 a 10. tente adivinhar')
-import random
-n = random.randint(0, 10)
-print(n)
+from random import randint
+computador = randint(0,10)
+print('Sou seu pc ... acabei de pensar em um número entre 0 a 10')
+print('Será que você consegue adivinha qual foi ? ')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou= True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos...Tente mais uma vez ')
+print(f'Acertou com quantas tentativas {palpites}. Parabéns <3')
