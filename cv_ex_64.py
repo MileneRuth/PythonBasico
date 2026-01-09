9ff8e553b9ea2213614328bbca5882befa35d1c0
MileneRuth <milene.ruth.ruth@outlook.com>
2026-01-09 12:21:31 -0300

    final da aula 14
    exercicios em python

---
+++ cv_ex_64.py
@@ -0,0 +1,6 @@
+n = 0
+cont = 0
+while n !=999:
+    n = int(input('Digite um numero inteiro, e caso queira parar digite 999: '))
+    soma = cont +1
+print('Fim e você digitou {} termos e o total foi de {}'.format(cont, soma))
