c2a51fb51c2ad50ce06ce9997e7e7650d6b94778
MileneRuth <milene.ruth.ruth@outlook.com>
2026-01-12 01:11:22 -0300

    ex 67
    tabuada

---
+++ cv_ex_67.py
@@ -0,0 +1,13 @@
+from time import sleep
+cont = n  = 0
+while True:
+    n = int(input('Digite um valor: '))
+    print(f'A tabuado do {n}')
+    sleep(2)
+    if  n > 0 :
+        for c in range(1,11): 
+         print(f'{n} x {c} = {n*c}')
+        
+    else:
+        break        
+print('Desculpe esse valor e invalido')
