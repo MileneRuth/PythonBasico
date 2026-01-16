a1874259b9ae3e82a908df90f8ce886c8013cef5
MileneRuth <milene.ruth.ruth@outlook.com>
2026-01-16 16:53:20 -0300

    ex 70

--- cv_ex_70.py
+++ cv_ex_70.py
@@ -2,15 +2,26 @@
 a) qual e o total gasto na compra 
 b) quantos produtos custam mais de R$1000
 c) Qual e o nome do produto mais barato '''
-tot = valorb = val = cont = 0
+tot = cont = total = menor = 0
 print('mercadinho da Ruth')
 while True:
     produto = str(input('Digite o nome do produto: '))
     valor = float(input('Digite agora o Preço do produto R$: '))
-    tot += valor
-    tot += 1
+    cont =+ 1
+    total  += valor
     if valor > 1000:
-            val += 1
-            if valor < valorb:
-                  cont += 1
-            
+         tot += 1
+    if cont == 1:
+        menor = valor
+    else:
+        if valor < menor:
+            menor = valor
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continua? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+            break
+print('{:-^40}'.format('Fim do programa'))
+print(f'O total da compra foi de {total:.2f}')
+print(f'Temos {tot} produtos  que custa mais de R$ 1.000,00.')
+print(f'O produto mais barato custa R${menor:.2f}')
