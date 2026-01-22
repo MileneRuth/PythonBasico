da2a10838fccf42127ffa443a22d6d8fec239e17
MileneRuth <milene.ruth.ruth@outlook.com>
2026-01-22 00:35:41 -0300

    correções

--- cv_ex_68.py
+++ cv_ex_68.py
@@ -1,18 +1,27 @@
-import random
-n = cont = opção = 0
-print('Jogo do Par ou Impar')
+from random import randint
+v = 0
 while True:
-    n = int(input('Digite um numero: '))
-    cl = str(input('E agora escolha uma opção [par] ou [impar] ? ')).lower
-    pc = random.randint(0,11)
-    print(f'O computador escolheu {pc}')
-    soma = n + pc 
-    if soma % 2 == 0:
-        opção = ['par']
-        print('Parabens você ganhou')
-        cont += 1
-    else:
-        break
-print(f'Fim você perdeu e teve o total de {cont} vitorias ')
-    
+    jogador =int(input('Digite um valor: '))
+    computador = randint(0,10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('par ou impar? [P/I]')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}')
+    if tipo == 'p':
+        if total % 2 == 0:
+            print('Você VENCEU')
+            v += 1
+        else:
+            print('Você PERDEU')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU')
+            v += 1
+        else:
+            print('Você PERDEU')
+            break
+    print('Vamos jogar novamente ? ')
+print(f'GAMER OVER você venceu {v} vezes consecutivas ')
     
--- cv_ex_69.py
+++ cv_ex_69.py
@@ -6,18 +6,21 @@ id = sex = cont = 0
 print('Olá preciso de algumas informações para cadastro')
 while True:
     idade = int(input('poderia me informa a idade: '))
-    sexo = str(input('poderia me informa o Sexo:[f][m] ')).lower()
-    if idade == 18:
-        id += 1
-    if sexo == 'm':
-        sex += 1
-    if sexo == 'f' and idade < 20:
-           cont += 1
-    opção = input('gostaria de continua? [s][n] ').lower()
-    if opção == 's':
-         continue
-    else:
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('poderia me informa o Sexo:[F/M] ')).strip().upper()[0]
+        if idade >= 18:
+            id += 1
+        if sexo =='M':
+            sex += 1
+        if sexo == 'F' and idade < 20:
+            cont +=1
+    opção = ' '
+    while opção not in 'SN':
+        opção = str(input('gostaria de continua? [S/N] ')).strip().upper()[0]
+    if opção == 'N':
          break
 print( f'Muito obrigado,\n Tivemos o total de {id} pessoas com 18 anos \n O total de homens cadastrados foi de {sex} \n Mulheres com menos de 20 anos temos {cont}')
+
    
     
